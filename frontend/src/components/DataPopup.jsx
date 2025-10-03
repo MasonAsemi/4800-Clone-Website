af1529da87efcc20af74e435b913098805215cb6
MasonAsemi <masonorton2005@gmail.com>
2025-10-03 10:20:23 -0700

    Reverting More Changes

--- frontend/src/components/DataPopup.jsx
+++ frontend/src/components/DataPopup.jsx
@@ -75,17 +75,17 @@ const DataPopup = ({ isOpen, onClose }) => {
         {loading && <p>Loading...</p>}
         {error && <p style={{color: 'red'}}>{error}</p>}
         
-        {!loading && !error && data.users.length > 0 && (
+        {!loading && !error && data.length > 0 && (
           <table border="1" style={{width: '100%', marginTop: '20px'}}>
             <thead>
               <tr>
-                {Object.keys(data.users[0]).map((key) => (
+                {Object.keys(data[0]).map((key) => (
                   <th key={key}>{key}</th>
                 ))}
               </tr>
             </thead>
             <tbody>
-              {data.users.map((row, index) => (
+              {data.map((row, index) => (
                 <tr key={index}>
                   {Object.values(row).map((value, i) => (
                     <td key={i}>{value?.toString() || 'N/A'}</td>
